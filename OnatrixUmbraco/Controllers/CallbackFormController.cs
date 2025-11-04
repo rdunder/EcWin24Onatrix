4c952e1725b4735f3f1d3b03663b4ebcaab280e0
rdunder <r.dunder@gmail.com>
2025-11-04 12:11:47 +0100

    Added callbackform with a model and controller for submit. added as partial view to about section.

---
+++ OnatrixUmbraco/Controllers/CallbackFormController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using OnatrixUmbraco.ViewModels;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace OnatrixUmbraco.Controllers;
+
+public class CallbackFormController : SurfaceController
+{
+    public CallbackFormController(
+        IUmbracoContextAccessor umbracoContextAccessor, 
+        IUmbracoDatabaseFactory databaseFactory, 
+        ServiceContext services, AppCaches appCaches, 
+        IProfilingLogger profilingLogger, 
+        IPublishedUrlProvider publishedUrlProvider) 
+        : base(
+            umbracoContextAccessor, 
+            databaseFactory, 
+            services, 
+            appCaches, 
+            profilingLogger, 
+            publishedUrlProvider)
+    {
+    }
+    
+    [HttpPost]
+    public IActionResult Submit(CallbackFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+        
+        // Work with form data here
+
+        return RedirectToCurrentUmbracoPage();
+    }
+}
