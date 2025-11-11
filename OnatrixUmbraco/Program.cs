f8c87421a22591e4e3e6d4334453419a8fb09578
rdunder <r.dunder@gmail.com>
2025-11-11 09:52:21 +0100

    Adjusted project to be able to run on ubuntu server behind nginx

--- OnatrixUmbraco/Program.cs
+++ OnatrixUmbraco/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using OnatrixUmbraco.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -6,13 +7,20 @@ builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
     .AddComposers()
-    .AddAzureBlobMediaFileSystem()
-    .AddAzureBlobImageSharpCache()
     .Build();
 
 builder.Services.AddScoped<FormSubmissionService>();
 
 WebApplication app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+    ForwardLimit = 2,
+    RequireHeaderSymmetry = false,
+    KnownNetworks = { },
+    KnownProxies = { }
+});
 
 await app.BootUmbracoAsync();
 
